9ec97a4ee03b0749094c46225d71a95be87a7171
Nhật Minh Phạm Đỗ <pdn.minh1999@outlook.com>
2019-06-08 11:53:11 +0700

    add cosine and tangent class

---
+++ BasicCalculatorCore/Operators/Cosine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorCore.Operators
+{
+    class Cosine : IOperable, IBiOperator
+    {
+        private object OpositeSide { get; set; }
+        private object HypothenuseSide { get; set; }
+        public object Calc(object OpositeSide, object HypothenuseSide)
+        {
+            this.OpositeSide = OpositeSide;
+            this.HypothenuseSide = HypothenuseSide;
+            if (OpositeSide == null || HypothenuseSide == null)
+            {
+                return null;
+            }
+            return (Double)OpositeSide / (Double)HypothenuseSide;
+        }
+
+        public string Info()
+            => OpositeSide == null || HypothenuseSide == null ? "Invalid parameters" : $"Cosin({OpositeSide},{HypothenuseSide})=";
+    }
+}
---
+++ BasicCalculatorCore/Operators/Tangent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorCore.Operators
+{
+    class Tangent : IOperable, IBiOperator
+    {
+        private object OpositeSide { get; set; }
+        private object AdjacentSide { get; set; }
+        public object Calc(object OpositeSide, object AdjacentSide)
+        {
+            this.OpositeSide = OpositeSide;
+            this.AdjacentSide = AdjacentSide;
+            if (OpositeSide == null || AdjacentSide == null)
+            {
+                return null;
+            }
+            return (Double)OpositeSide / (Double)AdjacentSide;
+        }
+
+        public string Info()
+            => OpositeSide == null || AdjacentSide == null ? "Invalid parameters" : $"Tangent({OpositeSide},{AdjacentSide})=";
+    }
+}
